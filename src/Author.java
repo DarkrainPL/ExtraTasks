fb45b60090e0637274d86447b114feb491092397
Karol <karolkaraszewski@gmail.com>
2024-01-26 17:59:23 +0100

    update 0.1v

---
+++ src/Author.java
@@ -0,0 +1,35 @@
+public class Author {
+    private String name;
+    private int age;
+    private String favouriteGenre;
+
+    public Author(String name, int age, String favouriteGenre) {
+        this.name = name;
+        this.age = age;
+        this.favouriteGenre = favouriteGenre;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFavouriteGenre() {
+        return favouriteGenre;
+    }
+
+    public void setFavouriteGenre(String favouriteGenre) {
+        this.favouriteGenre = favouriteGenre;
+    }
+}
---
+++ src/Library.java
@@ -0,0 +1,11 @@
+public class Library {
+
+
+    getBooksOfAuthor(String authorName){}
+    addBookToAuthor(String authorName, Book book){}
+    getAllAuthors(){}
+    getAllBooks(){}
+    getAllBooksAndAuthors(){}
+    addAuthor(Author author){}
+
+}
